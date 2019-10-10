5cc477c16d739f1296392be6dbeed06f86ae74e2
nhloder <nhloder@gmail.com>
2019-10-10 17:41:07 -0600

    farfromit

---
+++ myCode/index.js
@@ -0,0 +1,9 @@
+
+function addItem(){
+    const nameInput = document.querySelector('#expense-name')
+    amountInput = document.querySelector('#expense.amount')
+
+    let newItem = document.createElement('li')
+    newItem.classList.add('list-item')
+    
+}
